900085504b7b672e35a895de6b8dd130b6a366e3
LeoAldair <leo2.repa@gmail.com>
2022-03-23 15:12:46 -0600

    Commit para guardar el envio entre Fragments.

--- app/src/main/java/com/example/navegation/HomeFragment.kt
+++ app/src/main/java/com/example/navegation/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.navegation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -20,8 +21,9 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val navController = findNavController()
         binding.buttonHomeToInside.setOnClickListener {
-            //Toast.makeText(context, "prueba", Toast.LENGTH_SHORT).show()
-            navController.navigate(R.id.action_homeFragment_to_insideHome)
+            val array = arrayOf(binding.editTextName.text.toString(),binding.editTextFirstLastName.text.toString(),binding.editTextSecondLastName.text.toString(),binding.editTextUser.text.toString(),binding.editTextEmail.text.toString())
+            val direcction = HomeFragmentDirections.actionHomeFragmentToInsideHome(array)
+            findNavController().navigate(direcction)
         }
         return binding.root
     }
--- app/src/main/java/com/example/navegation/InsideHome.kt
+++ app/src/main/java/com/example/navegation/InsideHome.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.navegation.databinding.FragmentInsideHomeBinding
 
 
 class InsideHome : Fragment() {
-
+    private lateinit var binding: FragmentInsideHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inside_home, container, false)
+        val datos = arguments?.getStringArray("Datos")
+        binding = FragmentInsideHomeBinding.inflate(inflater, container, false)
+        if (datos != null){
+            binding.textNameValue.text = datos.get(0)
+            binding.textFirstLastNameValue.text = datos.get(1)
+            binding.textSecondLastNameValue.text = datos.get(2)
+            binding.textUserValue.text = datos.get(3)
+            binding.textEmailValue.text = datos.get(4)
+        }
+        return binding.root
     }
 
 }
